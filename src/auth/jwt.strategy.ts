82464e9ea62aeffd0a84072b94b09ec92af44bc4
x0lotl <danil200319771@gmail.com>
2025-07-08 15:28:57 +0300

    fix: same jwt secret key

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'your-secret-key', // In production, use environment variable
+      secretOrKey: 'secret_key',
     });
   }
 
